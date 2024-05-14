374233b8e302ff1afa03075259faf0ee18bb397f
Paul Mirtl <paul.mirtl@student.tugraz.at>
2024-05-14 13:33:23 +0200

    Amplitude Handler and Dataloader

---
+++ postprocessing/data/AmplitudeHandler.py
@@ -0,0 +1,251 @@
+from abc import abstractmethod
+from inspect import signature
+import typing
+
+import numpy as np
+
+from scipy.optimize import curve_fit
+from scipy.interpolate import CubicSpline
+
+from utils.fitfunctions import yukawa_potential_exp_sum
+from pwave.PartialWaveExpansion import PartialWaveExpansion
+from extrapolation.SchlessingerPointMethod import SchlessingerPointMethod
+
+from utils.fitfunctions import *
+
+
+class AmplitudeHandler:
+
+    def __init__(self, X, Z, f) -> None:
+        """
+            f   has shape (basis, X, Z)
+            X    has shape (X)
+            Z    has shape (Z)
+
+            fitfunc must be of the form callable(X, **params)
+        """
+
+        self.X = X
+        self.Z = Z
+        self.f = f
+
+        self.X_max = X[-1]
+
+        self.f_l = None             # shape (basis, l, X)
+
+        self.f_l_cspline = None     # list of CubicSpline() of shape (basis, l)
+
+        self.f_l_q = None           # shape (basis, l, q_gridpoints)
+
+        self.q = None
+
+
+    @abstractmethod
+    def f_l_fit(self, basis_idx, l, X):
+        pass
+
+    @abstractmethod
+    def fit_large_X_behaviour(self):
+        pass
+
+    
+    def interpolate_in_X(self):
+        self.f_l_cspline = [[CubicSpline(self.X, self.f_l[basis_idx, l]) for l in range(self.f_l.shape[1])] for basis_idx in range(self.f_l.shape[0])]
+    
+
+    def f_l_interpolation(self, basis_idx, l, X):
+        return self.f_l_cspline[basis_idx][l](X)
+    
+
+    def interpolate_in_q(self):
+        self.f_l_q_cspline = [[CubicSpline(self.q, self.f_l_q[basis_idx, l]) for l in range(self.f_l_q.shape[1])] for basis_idx in range(self.f_l_q.shape[0])]
+
+    
+    def f_l_q_interpolation(self, basis_idx, l, q):
+        return self.f_l_q_cspline[basis_idx][l](q)
+
+
+    def partial_wave_expand(self, degree_pwave_exp):
+        # f_l has shape (basis, l, X)
+        self.f_l = np.zeros((self.f.shape[0], degree_pwave_exp+1, self.f.shape[1]))
+
+        for basis_idx in range(self.f.shape[0]):
+            self.f_l[basis_idx, :, :] = PartialWaveExpansion(self.f[basis_idx, :], self.X, self.Z, degree_pwave_exp).get_f_x()
+
+
+    def partial_wave_expand_q(self, degree_pwave_exp: int, q_grid: np.ndarray, Z_grid: np.ndarray, fitonly: bool=False):
+        self.q = q_grid
+
+        f_q_generated = np.zeros((self.f.shape[0], len(q_grid), len(Z_grid)))
+        for basis_idx in range(f_q_generated.shape[0]):
+            for q_idx, q in enumerate(q_grid):
+                for Z_idx, Z in enumerate(Z_grid):
+                    f_q_generated[basis_idx, q_idx, Z_idx] = self.f_q_at(basis_idx, q, Z, fitonly=fitonly)
+
+        # f_l_q has shape (basis, l, num_q_gridpoints)
+        self.f_l_q = np.zeros((self.f.shape[0], degree_pwave_exp+1, len(q_grid)))
+
+        for basis_idx in range(f_q_generated.shape[0]):
+            self.f_l_q[basis_idx, :, :] = PartialWaveExpansion(f_q_generated[basis_idx, :], q_grid, Z_grid, degree_pwave_exp).get_f_x()
+        
+
+    def f_at(self, basis_idx, X, Z):
+        assert X >= 0
+        assert Z <= 1 and Z >= -1
+
+        res = 0.0
+
+        for l in range(self.f_l.shape[1]):
+            leg_coef_array = np.zeros(l + 1)
+            leg_coef_array[l] = 1.0
+
+            legpol_val = np.polynomial.legendre.legval(Z, leg_coef_array)     # TODO more efficient way
+
+            res += legpol_val * self.f_l_at(basis_idx, l, X)
+
+        return res
+    
+
+    def f_fit_at(self, basis_idx, X, Z):
+        assert X >= 0
+        assert Z <= 1 and Z >= -1
+
+        res = 0.0
+
+        for l in range(self.f_l.shape[1]):
+            leg_coef_array = np.zeros(l + 1)
+            leg_coef_array[l] = 1.0
+
+            legpol_val = np.polynomial.legendre.legval(Z, leg_coef_array)     # TODO more efficient way
+
+            res += legpol_val * self.f_l_fit(basis_idx, l, X)
+
+        return res
+
+
+    def f_l_at(self, basis_idx, l, X):
+        if X <= self.X_max:
+            ## avoid using spline --> try to stick to known X for X < X_max
+            #closest_idx = (np.abs(self.X - X)).argmin()
+            #if(np.isclose(self.X[closest_idx], X)):
+            #    return self.f_l[basis_idx, l, closest_idx]
+            #print(f"Needed interpolation in known region: X_query={X}, X_closest={self.X[closest_idx]} --> dif={np.abs(self.X[closest_idx] - X)}")
+
+            return self.f_l_interpolation(basis_idx, l, X)
+        
+        else:
+            return self.f_l_fit(basis_idx, l, X)
+        
+    
+    def fit_q_pwaves(self, q_fitfunc: typing.Callable, p0: typing.List, bounds: typing.Tuple, max_l: int=None, min_q: float=0):
+        self.q_fitfunc = q_fitfunc
+
+        self.f_l_q_fitcoeff = np.zeros((self.f_l_q.shape[0], self.f_l_q.shape[1], len(signature(self.q_fitfunc).parameters) - 1))
+
+        min_q_idx = np.argwhere(self.q >= min_q)[0][0]
+
+        for basis_idx in range(self.f_l_q.shape[0]):
+            for l in range(self.f_l_q.shape[1] if max_l is None else max_l):
+                popt, pcov = curve_fit(self.q_fitfunc, self.q[min_q_idx:], self.f_l_q[basis_idx, l, min_q_idx:], 
+                                    #p0=[1, 1, 0.5, 1, 1, 0, 1], 
+                                    #bounds=([0, 0, 0, 0, 0, 0, 0.5], [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, 8]), 
+                                    p0=p0,
+                                    bounds=bounds,
+                                    max_nfev=100000,
+                                    ftol=1E-15,
+                                    xtol=1E-15,
+                                    gtol=1E-15)
+                
+                self.f_l_q_fitcoeff[basis_idx, l, :] = popt
+        
+
+    @staticmethod
+    def X_at(q, Z):
+        # from q = M sqrt(2X) sqrt(1 - Z), we know that q has to be 0 if Z = 1, and thus also X = 0 from the inverse relation
+        if (Z == 1):
+            return 0
+        
+        M_nucleon = 0.94 # TODO pass
+        X = np.square(q) / (2.0 * np.square(M_nucleon) * (1.0 - Z))
+
+        return X
+
+
+    def f_q_at(self, basis_idx, q, Z, fitonly: bool=False):
+        if (fitonly):
+            return self.f_fit_at(basis_idx, self.X_at(q, Z), Z)
+        
+        else:
+            return self.f_at(basis_idx, self.X_at(q, Z), Z)
+        
+
+    def f_l_q_at(self, basis_idx, l, q):
+        if (q[0] < np.min(self.q) or q[-1] > np.max(self.q)):
+            raise Exception("Out of range for interpolated q --> TODO extrapolate")
+        
+        else:
+            return self.f_l_q_interpolation(basis_idx, l, q)
+        
+
+    def f_l_q_fit_at(self, basis_idx, l, q):
+        return self.q_fitfunc(q, *self.f_l_q_fitcoeff[basis_idx, l, :])
+
+        
+
+    
+        
+    
+
+class AmplitudeHandlerFitfunc(AmplitudeHandler):
+
+    def __init__(self, X, Z, f, fitfunc: typing.Callable[..., typing.Any] = yukawa_potential_exp_sum, p0: typing.List=None, bounds: typing.Tuple=None) -> None:
+        super().__init__(X, Z, f)
+
+        self.fitfunc = fitfunc
+        self.p0 = p0
+        self.bounds = bounds
+
+        self.f_l_fitcoeff = None    # shape (basis, l, #coefs fitfunction)
+
+
+    def fit_large_X_behaviour(self, max_l: int=None, min_X: float=0):
+        self.f_l_fitcoeff = np.zeros((self.f_l.shape[0], self.f_l.shape[1], len(signature(self.fitfunc).parameters) - 1))
+
+        min_X_idx = np.argwhere(self.X >= min_X)[0][0]
+
+        for basis_idx in range(self.f_l.shape[0]):
+            for l in range(self.f_l.shape[1] if max_l is None else max_l):
+                popt, pcov = curve_fit(self.fitfunc, self.X[min_X_idx:], self.f_l[basis_idx, l, min_X_idx:], 
+                                    #p0=[1, 1, 0.5, 1, 1, 0, 1], 
+                                    #bounds=([0, 0, 0, 0, 0, 0, 0.5], [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, 8]), 
+                                    p0=self.p0,
+                                    bounds=self.bounds,
+                                    max_nfev=100000,
+                                    ftol=1E-15,
+                                    xtol=1E-15,
+                                    gtol=1E-15)
+                
+                self.f_l_fitcoeff[basis_idx, l, :] = popt
+
+    
+    def f_l_fit(self, basis_idx, l, X):
+        return self.fitfunc(X, *self.f_l_fitcoeff[basis_idx, l, :])
+    
+
+
+class AmplitudeHandlerSchlessinger(AmplitudeHandler):
+
+    def __init__(self, X, Z, f) -> None:
+        super().__init__(X, Z, f)
+
+        self.f_l_schless = None     # shape (basis, l)  of type SchlessingerPointMethod()
+
+
+
+    def f_l_fit(self, basis_idx, l, X):
+        return self.f_l_schless[basis_idx][l].calc_value_at(X)
+
+
+    def fit_large_X_behaviour(self):
+        self.f_l_schless = [[SchlessingerPointMethod(self.X, self.f_l[basis_idx, l, :]) for l in range(self.f_l.shape[1])] for basis_idx in range(self.f_l.shape[0])]
+    
---
+++ postprocessing/data/Dataloader.py
@@ -0,0 +1,184 @@
+import os
+import json
+import typing
+
+import numpy as np
+import pandas as pd
+
+from basis.BasisTauToSymAsym import BasisTauToSymAsym
+
+
+class Dataloader:
+
+    def __init__(self, data_base_path: str, tensorbase_type: str, amplitude_isospin: int, process_type: str, dq_1_type: str, dq_2_type: str, Z_range, X_range_lower) -> None:
+        # Construct directory fitting specs
+        data_path = data_base_path + f"/BASE-{tensorbase_type}_I-{amplitude_isospin}_DQ-{dq_1_type}-{dq_2_type}/"
+
+        self.tensorbase_type = tensorbase_type
+        self.amplitude_isospin = amplitude_isospin
+        self.process_type = process_type
+        self.dq_1_type = dq_1_type
+        self.dq_2_type = dq_2_type
+
+
+        # Check process data is accessible
+        data_path_process = data_path + f"/{process_type}/"
+        if(not os.path.isdir(data_path_process)):
+            exit(-1)
+
+
+        # Find latest run
+        latest_run_dir_process = Dataloader.find_latest_run_dir(data_path_process)
+
+        if(latest_run_dir_process is None):
+            exit(-1)
+
+        data_path_process_latest = data_path_process + latest_run_dir_process + "/"
+        self.data_path = data_path_process_latest
+
+
+        # Check which basis we are working in
+        spec_file = open(data_path_process_latest + "/spec.json")
+        self.process_spec = json.load(spec_file)
+        spec_file.close()
+
+
+        # Sanity checks, specified base, dq type and so on should match
+        if(not self.process_spec["basis"] == tensorbase_type or 
+           not self.process_spec["amplitude_isospin"] == amplitude_isospin or 
+           not self.process_spec["diquark_type_1"] == dq_1_type or 
+           not self.process_spec["diquark_type_2"] == dq_2_type):
+            exit(-1)
+
+
+
+        # Load Dirac Space form factors and independent variables of process
+        self.pd_dirac_space_form_factors_list = Dataloader.load_dirac_space_form_factors(data_path_process_latest, tensorbase_type, Z_range, X_range_lower)
+        
+        # Load impulse grid
+        self.X, self.Z, self.X_unique, self.Z_unique = Dataloader.build_XZ_array_from_pd(self.pd_dirac_space_form_factors_list[0])
+
+        # Load h functions
+        self.h = Dataloader.build_matrix_from_pd_list(self.pd_dirac_space_form_factors_list, int(self.process_spec["X_points"]), int(self.process_spec["Z_points"]), "h")
+
+        # Base Transformation to other base in case we use the tau base
+        if (self.tensorbase_type == "tau"):
+            self.f = Dataloader.build_matrix_from_pd_list(self.pd_dirac_space_form_factors_list, int(self.process_spec["X_points"]), int(self.process_spec["Z_points"]), "f")
+            self.F = BasisTauToSymAsym.build_alternate_basis_numpy(self.f)
+        
+        elif (self.tensorbase_type == "T"):
+            self.F = Dataloader.build_matrix_from_pd_list(self.pd_dirac_space_form_factors_list, int(self.process_spec["X_points"]), int(self.process_spec["Z_points"]), "f")
+            self.f = BasisTauToSymAsym.build_alternate_basis_inverse_numpy(self.F)
+
+
+
+        # Load flavor factors
+        self.process_flavor_factor = Dataloader.load_flavor_space_form_factors("/home/past12am/OuzoCloud/Studium/Physik/6_Semester/SE_Bachelorarbeit/NNInteraction/flavorspace", process_type, amplitude_isospin, dq_1_type, dq_2_type)
+
+        # Build flavored dressing functions
+        self.f_flavored = self.f * self.process_flavor_factor
+        self.F_flavored = self.F * self.process_flavor_factor
+        
+
+
+    
+    @staticmethod
+    def build_matrix_from_pd_list(pd_scattering_list: typing.List, lenX: int, lenZ: int, colname: str):
+        col_vals_matrix = np.zeros((len(pd_scattering_list), len(pd.unique(pd_scattering_list[0]["X"])), len(pd.unique(pd_scattering_list[0]["Z"]))))
+
+        for basis_idx, pd_scattering in enumerate(pd_scattering_list):
+            pd_scattering = pd_scattering.sort_values(["X", "Z"], ascending=True)
+
+            X_idx = -1
+            Z_idx = -1
+
+            last_Z = -5
+            last_X = -5
+            for _, row in pd_scattering.iterrows():
+                if(row["X"] > last_X):
+                    X_idx += 1
+                    last_X = row["X"]
+
+                if(row["Z"] > last_Z):
+                    Z_idx += 1
+                    last_Z = row["Z"]
+
+                elif(row["Z"] < last_Z):
+                    Z_idx = 0
+                    last_Z = row["Z"]
+
+                col_vals_matrix[basis_idx, X_idx, Z_idx] = np.real(row[colname])
+
+        return col_vals_matrix
+
+
+
+
+    @staticmethod
+    def build_XZ_array_from_pd(pd_scattering: pd.DataFrame):
+        X_vals = pd_scattering["X"].to_numpy()
+        Z_vals = pd_scattering["Z"].to_numpy()
+
+        X_vals_unique = pd_scattering["X"].unique()
+        Z_vals_unique = pd_scattering["Z"].unique()
+
+        return X_vals, Z_vals, X_vals_unique, Z_vals_unique
+
+
+    @DeprecationWarning
+    def restrict_dataset(self, pd_ff, Z_range, X_range_lower):
+        idx_selector = np.where(np.logical_and(np.abs(np.array(pd_ff["Z"])) < Z_range, np.array(pd_ff["X"]) > X_range_lower))[0]
+        pd_ff = pd_ff.iloc[idx_selector]
+
+        return pd_ff
+
+
+    @staticmethod
+    def find_latest_run_dir(run_dirs_path: str):
+        latest_run_dir = None
+
+        run_dir_idx = -1
+        for run_directory in os.listdir(run_dirs_path):
+            if(not os.path.isdir(run_dirs_path + run_directory)):
+                continue
+
+            cur_run_dir_idx = int(run_directory[run_directory.find("_")+1 : ])
+            if(cur_run_dir_idx >= run_dir_idx):
+                latest_run_dir = run_directory
+                run_dir_idx = cur_run_dir_idx
+        
+        return latest_run_dir
+    
+
+    @staticmethod
+    def load_flavor_space_form_factors(flavor_data_path: str, process_type, amplitude_isospin, dq_1_type, dq_2_type):
+        pd_process_flavor = pd.read_csv(flavor_data_path + f"/{process_type}.csv", delimiter=";", decimal=".")
+
+        process_flavor_factor = pd_process_flavor[(pd_process_flavor.amplitude_isospin == amplitude_isospin) &
+                                                  (pd_process_flavor.diquark_1_type == dq_1_type) &
+                                                  (pd_process_flavor.diquark_2_type == dq_2_type)]["flavor_factor"].values[0]
+
+        try:
+            process_flavor_factor = float(process_flavor_factor)
+        except:
+            a , b = process_flavor_factor.split("/")
+            process_flavor_factor = int(a) / int(b)
+
+
+        return process_flavor_factor
+    
+
+    @staticmethod
+    def load_dirac_space_form_factors(data_path: str, tensorbase_type: str, Z_range, X_range_lower):
+
+        pd_dirac_form_factors = list()
+        for baseElemIdx in range(5):
+            pd_ff = pd.read_csv(data_path + f"/{tensorbase_type}_{baseElemIdx}.txt")
+            pd_ff = pd_ff.map(lambda s: complex(s.replace('i', 'j')) if(isinstance(s, str)) else s)
+
+            idx_selector = np.where(np.logical_and(np.abs(np.array(pd_ff["Z"])) <= Z_range, np.array(pd_ff["X"]) >= X_range_lower))[0]
+            pd_ff = pd_ff.iloc[idx_selector]
+
+            pd_dirac_form_factors.append(pd_ff)
+
+        return pd_dirac_form_factors
