5b032092eb8759f3f187b797d41676b827e84300
Paul <paul.mirtl@student.tugraz.at>
2025-05-06 15:57:21 +0200

    changed params

--- main.cpp
+++ main.cpp
@@ -67,13 +67,13 @@ int main(int argc, char *argv[])
     int numThreads = NUM_THREADS;
     int lenX = 8;
     int lenZ = 11;
-    int lenContourDef = 6;
+    int lenContourDef = 1;
 
 
-    int k2_integration_points = 100; // *2 for positive and neg. halfspace
-    int z_integration_points = 60;
-    int y_integration_points = 32;
-    int phi_integration_points = 20;
+    int k2_integration_points = 80; // *2 for positive and neg. halfspace
+    int z_integration_points = 40;
+    int y_integration_points = 40;
+    int phi_integration_points = 40;
 
 
     /* Working parameter sets
